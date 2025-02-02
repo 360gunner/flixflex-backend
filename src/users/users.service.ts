765823fb1704dcbae05e00f268b7b8b77276ea21
360gunner <hkentache@gmail.com>
2025-02-02 19:42:45 +0100

    define user and movie services

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas/user.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel('User') private userModel: Model<User>) {}
+
+  async create(user: User): Promise<User> {
+    const newUser = new this.userModel(user);
+    return newUser.save();
+  }
+
+  async findOne(username: string): Promise<User | null> {
+    return this.userModel.findOne({ username }).exec();
+  }
+
+  async addFavorite(userId: string, movieId: string): Promise<User | null> {
+    return this.userModel.findByIdAndUpdate(
+      userId,
+      { $addToSet: { favorites: movieId } },
+      { new: true },
+    );
+  }
+
+  async removeFavorite(userId: string, movieId: string): Promise<User | null> {
+    return this.userModel.findByIdAndUpdate(
+      userId,
+      { $pull: { favorites: movieId } },
+      { new: true },
+    );
+  }
+}
